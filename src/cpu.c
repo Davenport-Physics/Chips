8a35fc1d9d18f1f16afc6a3a69bdfedf721b6114
Emma Davenport <davenport.physics@gmail.com>
2019-04-29 21:01:24 -0500

    Fixed: I instead I_reg typo

--- src/cpu.c
+++ src/cpu.c
@@ -146,7 +146,7 @@ void ExecuteNextOpCode()
         if (sound_timer == 1) {
 
             PlayBeep();
-            
+
         }
         sound_timer--;
 
@@ -572,7 +572,7 @@ void AddVx_To_I(unsigned short opcode)
 void Set_I_ToLocationOfSprite(unsigned short opcode) 
 {
 
-    I = v_regs[GetNibble(opcode, 2)] * 5;
+    I_reg = v_regs[GetNibble(opcode, 2)] * 5;
 
 }
 
